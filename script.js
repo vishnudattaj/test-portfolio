a42c47a929838470a68c83e73d7b0392a8c4cbf0
Vishnu Jayanti <vishnudattaj@gmail.com>
2026-06-23 21:21:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ async function simulateCompilation(element, duration) {
   element.className = 'compile';
   while (Date.now() - start < duration) {
     element.textContent = Array.from({length: 15}).map(() => chars[Math.floor(Math.random() * chars.length)]).join('');
-    await sleep(100);
+    await sleep(60);
   }
   element.textContent = '';
   element.className = 'out';
